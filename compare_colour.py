a310b772c25377e66d2f265294e00dcb1397f9cf
J3mboJ0e <mainwaring.joey@gmail.com>
2026-01-17 17:32:55 -0500

    Create compare_colour.py
    Database Management Course - Simple python script, using a single if statement, to guess a single colour.

---
+++ compare_colour.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+    Program: compare_colour.py
+    Purpose: to evaluate a matching colout
+    Date and version: Sept 15, 2025 - version 1
+    Author: Joey Mainwaring
+"""
+
+#User input for colour
+algonquin_colour = input("What is Alongquin College's colour: ")
+
+#Evaluation of matching colour
+if algonquin_colour.lower() == "green":
+    print("That is correct!")
+else:
+    print("That is not correct :(")
+
+#Closing Statement
+print("Algonquin College's team colour is green")
