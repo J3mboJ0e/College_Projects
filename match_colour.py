164924c3607f53ea8947f58fef6ea2445cf5f342
J3mboJ0e <mainwaring.joey@gmail.com>
2026-01-17 17:36:43 -0500

    Create match_colour.py
    Database Management Course - Simple python script, using a single if statement, to match a specific colour

---
+++ match_colour.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+    Program name: match_colour.py
+    Program purpose: Evaluate a matching colour
+    Date and version:: Sept 15, 2025 - version 1
+    Author: Joey Mainwaring
+"""
+
+# Prompt user for colour input
+algonquin_colour = input("What is Algonquin College's colour: ")
+
+# Match the colour
+if algonquin_colour.lower() == "green":
+    print("That is correct")
+
+#Closing message
+print("Algonquin College team colour is green")
